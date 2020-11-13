0db71b1798414ad5a3ccc9a0da9731660dfbac72
SK0M0R0H <arkonit-t@yandex.ru>
2020-11-13 23:39:47 +0200

    add into_affine log

--- src/groth16/prover.rs
+++ src/groth16/prover.rs
@@ -557,10 +557,17 @@ where
                 g_c.add_assign(&h.wait()?);
                 g_c.add_assign(&l.wait()?);
 
+                info!("Calculate g_a");
+                let p_a = g_a.into_affine();
+                info!("Calculate g_b");
+                let p_b =g_b.into_affine();
+                info!("Calculate g_c");
+                let p_c = g_c.into_affine();
+
                 Ok(Proof {
-                    a: g_a.into_affine(),
-                    b: g_b.into_affine(),
-                    c: g_c.into_affine(),
+                    a: p_a,
+                    b: p_b,
+                    c: p_c,
                 })
             },
         )
