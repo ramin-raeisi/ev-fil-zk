f39683d9d0bc6122dcfc5a6f570a51481b8487b3
Ilia Shirobokov <arkonit-t@yandex.ru>
2021-01-27 16:16:02 -0500

    use cpu multiexp for small values

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -267,9 +267,9 @@ impl<E> MultiexpKernel<E>
 
         // use cpu for parallel calculations
         let mut cpu_n = ((n as f64) * get_cpu_utilization()) as usize;
-        /*if n < 1000 {
+        if n < 10000 {
             cpu_n = n;
-        }*/
+        }
         let n = n - cpu_n;
 
         for p in scheduler::DEVICE_POOL.devices.iter() {
