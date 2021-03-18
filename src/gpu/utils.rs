17106c057a7b9517bbb2e31901f114c5a50b5f17
Mikhail Komarov <nemo@nil.foundation>
2021-03-18 13:22:45 +0300

    RTX A6000 naming fixed.

--- src/gpu/utils.rs
+++ src/gpu/utils.rs
@@ -17,6 +17,7 @@ lazy_static::lazy_static! {
 
             ("TITAN RTX".to_string(), (4608, 0)),
 
+            ("RTX A6000".to_string(), (10752, 134217728)),
             ("Tesla V100".to_string(), (5120, 67108864)),
             ("Tesla V100S".to_string(), (5120, 67108864)),
             ("Tesla P100".to_string(), (3584, 0)),
@@ -30,7 +31,6 @@ lazy_static::lazy_static! {
             ("GeForce RTX 3080".to_string(), (8704, 33554466)),
             ("GeForce RTX 3080 20GB".to_string(), (8704, 0)),
             ("GeForce RTX 3090".to_string(), (10496, 67108864)),
-            ("GeForce RTX A6000".to_string(), (10752, 134217728)),
 
             ("GeForce RTX 2080 Ti".to_string(), (4352, 0)),
             ("GeForce RTX 2080 SUPER".to_string(), (3072, 0)),
