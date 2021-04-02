52057e8a45cbb0857a24363a1d3382a99445a6e4
Ilia Shirobokov <arkonit-t@yandex.ru>
2021-04-01 20:03:53 -0400

    rename memory padding variable

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -39,11 +39,11 @@ pub fn get_cpu_utilization() -> f64 {
 }
 
 pub fn get_memory_padding() -> f64 {
-    std::env::var("FIL_ZK_GPU_MEMORY_PADDING")
+    std::env::var("C2_GPU_MEMORY_PADDING")
         .and_then(|v| match v.parse() {
             Ok(val) => Ok(val),
             Err(_) => {
-                error!("Invalid FIL_ZK_GPU_MEMORY_PADDING! Defaulting to {}", MEMORY_PADDING);
+                error!("Invalid C2_GPU_MEMORY_PADDING! Defaulting to {}", MEMORY_PADDING);
                 Ok(MEMORY_PADDING)
             }
         })
