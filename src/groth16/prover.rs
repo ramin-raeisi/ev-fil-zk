c08f518d5662239605c4fbb51bf0f85834277dd2
Luannet <agreeru@mail.ru>
2021-04-13 03:02:16 +0200

    remove info

--- src/groth16/prover.rs
+++ src/groth16/prover.rs
@@ -272,14 +272,10 @@ impl<E: Engine> ConstraintSystem<E> for ProvingAssignment<E> {
     fn align_variable(&mut self, v: &mut Variable, input_shift: usize, aux_shift: usize,) {
         match v {
             Variable(Index::Input(i)) => {
-                info!("index = {}, new index = {}", i, input_shift);
-                //*v = Variable(Index::Input(self.input_assignment.len() + *i - input_shift));
                 *v = Variable(Index::Input(input_shift));
 
             }
             Variable(Index::Aux(i)) => {
-                info!("index = {}, new index = {}", i , aux_shift);
-                //*v = Variable(Index::Aux(self.aux_assignment.len() + *i - aux_shift));
                 *v = Variable(Index::Aux(aux_shift));
 
             }
